0d905ed0759828d325019eaca05ac094bbbea727
abdulhadi3115 <abhad9213@gmail.com>
2019-03-15 11:43:49 +0700

    jawaban bmi

---
+++ index.php
@@ -0,0 +1,159 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>BMI Calculator</title>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+<!--===============================================================================================-->
+	<link rel="icon" type="image/png" href="images/icons/favicon.ico"/>
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="vendor/bootstrap/css/bootstrap.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="fonts/font-awesome-4.7.0/css/font-awesome.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="vendor/animate/animate.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="vendor/css-hamburgers/hamburgers.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="vendor/animsition/css/animsition.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="vendor/select2/select2.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="vendor/daterangepicker/daterangepicker.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="css/util.css">
+	<link rel="stylesheet" type="text/css" href="css/main.css">
+<!--===============================================================================================-->
+</head>
+<body>
+
+
+	<div class="container-contact100" style="background-image: url(http://1.bp.blogspot.com/_Y5VHIIGTJ1c/TQwx9UNBKqI/AAAAAAAAUzY/dVTjet8Re7k/s1600/0803_02_1024-753704.jpg);">
+		<div class="wrap-contact100 p-l-55 p-l-55 p-r-55 p-t-80 p-b-30">
+			<form action=index.php method=post>
+				<span class="contact100-form-title p-b-37">
+					<h2>
+					Body Mass Index
+					</h2>
+				</span>
+
+				<div class="wrap-input100 validate-input" data-validate="Angka is required">
+					<span class="label-input100">Berat</span>
+					<input class="input100" type="text" name="berat" placeholder="Berat Anda (Kg)" value="<?php if(!empty($_POST['berat'])){ echo $_POST['berat']; }?>" required>>
+					<span class="focus-input100"></span>
+				</div>
+
+				<div class="wrap-input100 validate-input" data-validate = "Valid email is required: ex@abc.xyz">
+					<span class="label-input100">Tinggi</span>
+					<input class="input100" type="text" name="tinggi" placeholder="Tinggi Anda (m)" value="<?php if(!empty($_POST['tinggi'])){ echo $_POST['tinggi']; }?>" required>
+					<span class="focus-input100"></span>
+				</div>
+
+				<!-- <div class="wrap-input100 input100-select">
+					<span class="label-input100">Needed Services</span>
+					<div>
+						<select class="selection-2" name="service">
+							<option>Choose Services</option>
+							<option>Online Store</option>
+							<option>eCommerce Bussiness</option>
+							<option>UI/UX Design</option>
+							<option>Online Services</option>
+						</select>
+					</div>
+					<span class="focus-input100"></span>
+				</div> -->
+
+				<!-- <div class="wrap-input100 input100-select">
+					<span class="label-input100">Budget</span>
+					<div>
+						<select class="selection-2" name="budget">
+							<option>Select Budget</option>
+							<option>1500 $</option>
+							<option>2000 $</option>
+							<option>2500 $</option>
+						</select>
+					</div>
+					<span class="focus-input100"></span>
+				</div> -->
+
+				<!-- <div class="wrap-input100 validate-input" data-validate = "Message is required">
+					<span class="label-input100">Message</span>
+					<textarea class="input100" name="message" placeholder="Your message here..."></textarea>
+					<span class="focus-input100"></span>
+				</div> -->
+
+				<div class="container-contact100-form-btn">
+					<div class="wrap-contact100-form-btn">
+						<div class="contact100-form-bgbtn"></div>
+						<button class="contact100-form-btn" type="submit" name="hitung" value="hitung">
+							<span>
+								Calculate BMI
+								<i class="fa fa-long-arrow-right m-l-7" aria-hidden="true"></i>
+							</span>
+						</button>
+					</div>
+				</div>
+			</form>
+
+			<?php
+				if(!empty($_POST['hitung'])) { 
+			    $berat = $_POST['berat'];
+			    $tinggi = $_POST['tinggi'];
+			    function bmi($berat,$tinggi) {
+			        $bmi = $berat/($tinggi*$tinggi);
+			        return $bmi;
+			    }   
+			    $bmi = bmi($berat,$tinggi);
+			    if ($bmi <= 18.5) {
+			        $output = "UNDERWEIGHT";
+			        } else if ($bmi > 18.5 AND $bmi<=24.9 ) {
+			        $output = "NORMAL WEIGHT";
+			        } else if ($bmi > 24.9 AND $bmi<=29.9) {
+			        $output = "OVERWEIGHT";
+			        } else if ($bmi > 30.0) {
+			        $output = "OBESE";
+			    }
+			    echo "Your BMI value is  " . $bmi . "<br>  and you are : "; 
+			    echo "$output";
+			}
+			?>
+		</div>
+	</div>
+
+	<div id="dropDownSelect1"></div>
+
+<!--===============================================================================================-->
+	<script src="vendor/jquery/jquery-3.2.1.min.js"></script>
+<!--===============================================================================================-->
+	<script src="vendor/animsition/js/animsition.min.js"></script>
+<!--===============================================================================================-->
+	<script src="vendor/bootstrap/js/popper.js"></script>
+	<script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+<!--===============================================================================================-->
+	<script src="vendor/select2/select2.min.js"></script>
+	<script>
+		$(".selection-2").select2({
+			minimumResultsForSearch: 20,
+			dropdownParent: $('#dropDownSelect1')
+		});
+	</script>
+<!--===============================================================================================-->
+	<script src="vendor/daterangepicker/moment.min.js"></script>
+	<script src="vendor/daterangepicker/daterangepicker.js"></script>
+<!--===============================================================================================-->
+	<script src="vendor/countdowntime/countdowntime.js"></script>
+<!--===============================================================================================-->
+	<script src="js/main.js"></script>
+
+	<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-23581568-13"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-23581568-13');
+</script>
+
+</body>
+</html>
